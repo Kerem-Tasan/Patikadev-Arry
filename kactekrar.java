238e9b52b0e83d4b881169e69f426ea4a72d74dc
Kerem-Tasan <128915083+Kerem-Tasan@users.noreply.github.com>
2023-05-06 19:57:40 +0300

    Add files via upload

---
+++ kactekrar.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+
+public class kactekrar {
+
+    static boolean isFind(int arr[], int value) {
+        for (int i : arr) {
+
+            if (i == value) {
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] list = {3, 5, 6, 8, 12, 5, 123, 12, 5, 12, 12, 65432, 21, 65432};
+        int sayac = 1;
+        int[] duplicate = new int[list.length];
+        System.out.println(Arrays.toString(list));
+        Arrays.sort(list);
+        System.out.println("Sıralı hali:\n" + Arrays.toString(list));
+        System.out.println("Tekrar sayıları:");
+        for (int i = 0; i < list.length; i++) {
+            if (i == 0) {
+                for (int j=0;j< list.length;j++) {
+                    if (i != j && list[i] == list[j]) {
+                        sayac++;
+                    }
+                }
+                System.out.println(list[i] + " sayısı " + sayac + " defa tekrar edildi.");
+            } else if (list[i] != list[i - 1]) {
+                for (int j = 0; j < list.length; j++) {
+                    if (i != j && list[i] == list[j]) {
+                        sayac++;
+                    }
+                }
+                System.out.println(list[i] + " sayısı " + sayac + " kadar tekrar edildi.");
+            }
+                sayac = 1;
+        }
+    }
+}
